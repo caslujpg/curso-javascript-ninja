258562c92f4f7c5c0a16ae5b31872d7aed4b9646
Lucas <lucasleonardo2000@hotmail.com>
2022-02-20 21:50:08 -0300

    Challenge 07

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -16,7 +16,7 @@ function addItem(y){
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-addItem([1, 2, 3]);
+addItem([1, "lul", false]);
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
